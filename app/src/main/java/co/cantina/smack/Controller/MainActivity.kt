271306cb570355441160d4c0ac1020760c16cccc
Shukti Shaikh <shukti.azad@gmail.com>
2019-04-05 09:41:07 -0400

    getting user login details from broadcast receiver

--- app/src/main/java/co/cantina/smack/Controller/MainActivity.kt
+++ app/src/main/java/co/cantina/smack/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package co.cantina.smack.Controller
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.support.v4.content.LocalBroadcastManager
 import android.support.v4.view.GravityCompat
@@ -12,6 +13,7 @@ import android.view.View
 import co.cantina.smack.R
 import co.cantina.smack.Services.AuthService
 import co.cantina.smack.Services.UserDataService
+import co.cantina.smack.Utilities.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 import kotlinx.android.synthetic.main.nav_header_main.*
@@ -32,7 +34,8 @@ class MainActivity : AppCompatActivity() {
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        LocalBroadcastManager.getInstance(this).registerReceiver()
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDataChangeReceiver, IntentFilter(
+            BROADCAST_USER_DATA_CHANGE))
 
     }
 
