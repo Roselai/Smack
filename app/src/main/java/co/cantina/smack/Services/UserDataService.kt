af1522d0aca662ce38c5713e07a01bbf7e2b5ac7
Shukti Shaikh <shukti.azad@gmail.com>
2019-04-05 10:00:21 -0400

    added function to convert avatar color into rgb values

--- app/src/main/java/co/cantina/smack/Services/UserDataService.kt
+++ app/src/main/java/co/cantina/smack/Services/UserDataService.kt
@@ -1,10 +1,37 @@
 package co.cantina.smack.Services
 
+import android.graphics.Color
+import java.util.*
+
 object UserDataService {
     var id = ""
     var avatarColor = ""
     var avatarName = ""
     var email = ""
     var name = ""
+
+    fun returnAvatarColor(components: String) : Int {
+        // "[0.21568627450980393, 0.7176470588235294, 0.5294117647058824, 1]"
+        // 0.21568627450980393 0.7176470588235294 0.5294117647058824 1
+
+        val strippedColor = components
+            .replace("[", "")
+            .replace("]", "")
+            .replace(",", "")
+
+        var r = 0
+        var g = 0
+        var b = 0
+
+        val scanner = Scanner(strippedColor)
+        if (scanner.hasNext()){
+            r = (scanner.nextDouble() * 255).toInt()
+            g = (scanner.nextDouble() * 255).toInt()
+            b = (scanner.nextDouble() * 255).toInt()
+        }
+
+        return Color.rgb(r, g, b)
+    }
+
 
 }
